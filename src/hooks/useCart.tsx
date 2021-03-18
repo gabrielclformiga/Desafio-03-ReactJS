4eb72a338dd840c31d22ef65e30a4beb00dd8a4d
Gabriel Cortez <gabrielclformiga@gmail.com>
2021-03-18 20:54:28 -0300

    Challenge Completed

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,77 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const { data: productFromStock } = await api.get(`stock/${productId}`);
+
+      const uptadedCart = [...cart];
+
+      const productInCart = uptadedCart.find((product) => product.id === productId);
+
+      if (!productInCart) {
+        const { data: productFromProducts } = await api.get<Product>(`products/${productId}`);
+
+        uptadedCart.push({...productFromProducts, amount: 1});
+
+        setCart(uptadedCart);
+
+        localStorage.setItem(
+          '@RocketShoes:cart',
+          JSON.stringify(uptadedCart)
+        );
+
+        return;
+      }
+
+      if (productFromStock.amount > productInCart.amount) {
+
+        uptadedCart.push({...productInCart, amount: productInCart.amount + 1});
+
+        setCart(uptadedCart);
+
+        localStorage.setItem(
+          '@RocketShoes:cart',
+          JSON.stringify(uptadedCart)
+        );
+      } else {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
       // TODO
-    } catch {
-      // TODO
+      const uptadedCart = [...cart];
+
+      const cartProductIndex = uptadedCart.findIndex(product => product.id === productId);
+
+      const productInCart = uptadedCart[cartProductIndex];
+
+      if (!productInCart) {
+        throw new Error;
+      }
+
+      uptadedCart.splice(cartProductIndex, 1);
+
+      setCart(uptadedCart);
+
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(uptadedCart));
+    } catch(error) {
+      toast.error("Erro na remoção do produto");
     }
   };
 
@@ -53,9 +102,30 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      const { data: stockProduct } = await api.get<Product>(`stock/${productId}`);
+
+      if ((stockProduct.amount < amount) || (amount < 1)) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      let uptadedCart = [...cart];
+
+      uptadedCart = uptadedCart.map((product) => {
+        return product.id === productId ?
+          { ...product, amount: amount }
+          : product
+      });
+
+      // [{"amount": 2, "id": 1, "image": "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg", "price": 179.9, "title": "Tênis de Caminhada Leve Confortável"}, {"amount": 1, "id": 2, "image": "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg", "price": 139.9, "title": "Tênis VR Caminhada Confortável Detalhes Couro Masculino"}]
+
+      // [{"amount": 2, "id": 1, "image": "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg", "price": 179.9, "title": "Tênis de Caminhada Leve Confortável"}, {"amount": 0, "id": 2, "image": "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg", "price": 139.9, "title": "Tênis VR Caminhada Confortável Detalhes Couro Masculino"}]
+
+      setCart(uptadedCart);
+
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(uptadedCart));
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
